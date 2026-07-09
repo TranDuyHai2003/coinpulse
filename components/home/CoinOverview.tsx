c52563239994b08c0868579e7574decae857d2c3
@Haitd2003 <haitd01@ringme.vn>
2026-07-09 14:23:23 +0700

    implement candlestick chart

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -3,30 +3,53 @@ import fetcher from "@/lib/coingecko.actions";
 import Image from "next/image";
 import { formatCurrency } from "@/lib/utils";
 import { CoinOverviewFallback } from "@/components/home/fallback";
+import CandleStickChart from "@/components/CandleStickChart";
 
 const CoinOverview = async () => {
-  let coin;
+  // 1. Khai báo biến (và kiểu dữ liệu) ở ngoài scope để dùng cho việc render bên dưới
+  let coin: CoinDetailsData;
+  let coinOHLCData: OHLCData[];
+
   try {
-    coin = await fetcher<CoinDetailsData>("coins/bitcoin", {
-      dex_pair_format: "symbol",
-    });
+    // 2. Gọi API và gán trực tiếp vào biến đã khai báo (KHÔNG dùng thêm const/let ở đây nữa)
+    [coin, coinOHLCData] = await Promise.all([
+      fetcher<CoinDetailsData>("coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+      fetcher<OHLCData[]>("/coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: 1,
+        // interval: "hourly",
+        precisionL: "full",
+      }),
+    ]);
   } catch (error) {
+    // 3. Nếu lỗi, bắt và return Fallback ngay lập tức
     console.error("Error fetching coin details:", error);
     return <CoinOverviewFallback />;
   }
 
+  // 4. Render JSX bình thường vì lúc này coin và coinOHLCData chắc chắn đã có data
   return (
     <div id="coin-overview">
-      <div className="header">
-        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-        <div className="info">
-          <p>
-            {coin.name}/{coin.symbol.toUpperCase()}
-          </p>
-          <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+      <CandleStickChart data={coinOHLCData} coinId="bitcoin">
+        <div className="header pt-2">
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={56}
+            height={56}
+          />
+          <div className="info">
+            <p>
+              {coin.name}/{coin.symbol.toUpperCase()}
+            </p>
+            <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+          </div>
         </div>
-      </div>
+      </CandleStickChart>
     </div>
   );
 };
+
 export default CoinOverview;
